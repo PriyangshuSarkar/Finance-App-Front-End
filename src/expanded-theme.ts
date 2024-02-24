898edc4a3c587ecf8babf3bb7e5e6a791218e8f5
PriyangshuSarkar <priyangshusarkar0@gmail.com>
2024-02-24 22:42:14 +0530

    Final

--- src/expanded-theme.ts
+++ src/expanded-theme.ts
@@ -1,5 +1,6 @@
 // eslint-disable-next-line
 // @ts-ignore
+// eslint-disable-next-line
 import { Palette, PaletteColor } from "@mui/material/styles/createPalette";
 
 declare module "@mui/material/styles/createPalette" {
